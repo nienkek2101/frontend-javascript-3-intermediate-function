c7475ad90d7a11f21250cd6353f1dba0e7717fcc
nienkek2101 <nienke.kapers@gmail.com>
2021-10-28 09:29:02 +0200

    ik kom er niet uit-tot eind 25-10

--- functions-methods.js
+++ functions-methods.js
@@ -10,7 +10,30 @@
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
 
+// Dit was mijn poging!!
+// function getEmailDomain(email) {
+//     // geeft string na @ t/m laatste karakter
+//     return getEmailDomain.substring(getEmailDomain.lastIndexOf("@"), getEmailDomain.length)
+// }
+//
+// const domainName = getEmailDomain("nienke.kapers@gmail.com")
+// console.log(domainName);
 
+function getEmailDomain(emailadress) {
+    // bepaal op welk indexnummer het apenstaartje staat
+    const indexOfApenstaartje = emailadress.indexOf("@");
+    // knip alles na het apenstaartje van het emailadres af
+    const domain = emailadress.substring(indexOfApenstaartje + 1);
+    return domain;
+}
+
+const domainOne = getEmailDomain("nienke.kapers@gmail.com");
+const domainTwo = getEmailDomain("n.eeken@novi-education.nl");
+const domainThree = getEmailDomain("nienke@kapers.eu");
+
+console.log(domainOne);
+console.log(domainTwo);
+console.log(domainThree);
 
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of extern domein (zoals gmail of outlook)
@@ -20,6 +43,25 @@
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+function typeOfEmail(emailadress) {
+// als e-mail na @ novi-education.nl bevat --> log dan Student
+// als e-mail na @ novi.nl bevat --> log dan Medewerker
+// als e-mail geen van beide bevat --> log dan Extern
+    const indexOfApenstaartje = emailadress.indexOf("@");
+    const domain = emailadress.substring(indexOfApenstaartje +1);
+    if (domain.includes("novi.nl")) {
+        console.log("Medewerker");
+    } else if (domain.includes("novi-education.nl")) {
+        console.log("Student");
+    } else {
+        console.log("Extern");
+    }
+}
+
+const emailOne = typeOfEmail("n.eeken@novi-education.nl");
+const emailTwo = typeOfEmail("t.meilink@novi.nl");
+const emailThree = typeOfEmail("a.wiersma@outlook.com");
+const emailFour = typeOfEmail("novi.nlaapjesk@outlook.com")
 
 
 /* Opdracht  3 */
@@ -34,3 +76,35 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+function checkEmailValidity(emailadress) {
+    // bevatApenstaartje moet true zijn
+    // bevatKomma moet false zijn
+    // laatstePunt moet false zijn
+
+    const bevatApenstaartje = emailadress.includes("@");
+    const bevatKomma = emailadress.includes(",");
+    const aantalKarakters = emailadress.length;
+    const laatstePunt = aantalKarakters.includes(".");
+
+    console.log(aantalKarakters);
+    if ((bevatApenstaartje = true) && (bevatKomma = false ) && (laatstePunt = false)) {
+        console.log("Dit e-mailadres klopt");
+    } else {
+        console.log("Dit e-mailadres klopt niet")
+    }
+
+    // if (emailadress.includes("@") && emailadress.includes(",") = false) {
+    //     console.log("Yes");
+    // } else {
+    //     console.log("No");
+    // }
+}
+
+
+
+const emailFive = checkEmailValidity("n.eeken@novi-education.nl");
+const emailSix = checkEmailValidity("tessameilink@novi.nl");
+const emailSeven = checkEmailValidity("n.eekenanovi.nl");
+const emailEight = checkEmailValidity("n.eeken@novinl.");
+const emailNine = checkEmailValidity("tessmellink@novi,nl");
